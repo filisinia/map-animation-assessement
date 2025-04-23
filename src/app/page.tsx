ff12331d80d194a607e0fdeba898e5eb732d19c4
Mariia Petrenko <filisinia@gmail.com>
2025-04-23 14:23:40 +0300

    feat: long walks component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,20 @@ export default function Home() {
         start={{ xPercent: 49.5, yPercent: 61.5 }}
         end={{ xPercent: 91.5, yPercent: 40 }}
       />
+
+      <div className="absolute bottom-[20%] right-[8%] w-56 flex flex-col gap-3">
+        <InfoMessage type="error" time="30-15" title="Airlines">
+          Prolonged disembarking
+          <span className="text-primary/60"> from the airplane</span>
+        </InfoMessage>
+
+        <MessageWrapper className="h-16 font-bold text-sm">
+          <p>
+            Long walks
+            <span className="text-primary/60"> to the airport exit</span>
+          </p>
+        </MessageWrapper>
+      </div>
     </div>
   );
 }
